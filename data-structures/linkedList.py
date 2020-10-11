6dd5a97d83c85241acc3c4370241fb276bb1ac86
Apurba Panja <apurbapanja1@gmail.com>
2020-10-11 12:13:31 +0530

    Add a new linkedList.py script
    This script contains a class named "LinkedList which implements a singly linked list.
    add two methods to this class
    1. append(value) => this method adds a value to the end of the linked list
    2. prepend(value) => this method adds a value to the beginning of the linked list.

---
+++ data-structures/linkedList.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = {
+            'value': value,
+            'next': None
+        }
+        self.tail = self.head
+        self.length = 1
+    def prepend(self, value):
+        new_node = {
+            'value': value,
+            'next': self.head
+        }
+        self.head = new_node
+        self.length += 1
+    def append(self, value):
+        new_node = {
+            'value': value,
+            'next': None
+        }
+        self.tail['next'] = new_node
+        self.tail = new_node
+        self.length += 1
+
+my_linked_list = LinkedList(10)
+my_linked_list.prepend(6)
+my_linked_list.prepend(22)
+my_linked_list.append(26)
+
+print(f"length: {my_linked_list.length}\nhead: {my_linked_list.head}\ntail: {my_linked_list.tail}")
