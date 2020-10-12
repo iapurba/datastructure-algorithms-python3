a10d83341230d0418afb0dc0dc4f9369ca17e76c
Apurba Panja <apurbapanja1@gmail.com>
2020-10-12 13:22:41 +0530

    Add stack.py script
    this script contains implementation of stack using array and linked list.

---
+++ data-structures/stack.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+# way1: implementation of stack using linked list
+class Stack1:
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def peek(self):
+        """Return top item of the stack"""
+        return self.top
+
+    def push(self, value):
+        """Add new element on top of the stack"""
+        new_node = {
+            'value': value,
+            'next': None
+        }
+        if self.length == 0:
+            self.top = new_node
+            self.bottom = new_node
+        else:
+            new_node['next'] = self.top
+            self.top = new_node
+        self.length += 1
+        return self
+
+    def pop(self):
+        """Remove the top element from the stack"""
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            top_node = self.top
+            self.top = None
+            self.bottom = None
+            del top_node
+            self.length -= 1
+            return self
+        else:
+            top_node = self.top
+            self.top = self.top['next']
+            del top_node
+            self.length -= 1
+            return self
+
+    def search(self, value):
+        """Return True if the given item exists in the stack, if not returns False"""
+        current_node = self.top
+        while(current_node):
+            if current_node['value'] == value:
+                return True
+            current_node = current_node['next']
+        return False
+
+    def isEmpty(self):
+        """Returns True if stack is empty, False otherwise"""
+        if self.length == 0:
+            return True
+        return False
+
+# way2: implementation of stack using Array
+class Stack2:
+    def __init__(self):
+        self.stack = []
+
+    def peek(self):
+        length = len(self.stack)
+        if length > 0:
+            return self.stack[length - 1]
+        return None
+
+    def push(self, value):
+        self.stack.append(value)
+        return self
+
+    def pop(self):
+        if len(self.stack) > 0:
+            self.stack.pop()
+            return self
+        return None
+
+
+# instantiated Stack1 class
+website = Stack1()
+
+## stack operations
+# website.push('airbnb')
+# website.push('amazon')
+# website.push('netflix')
+# website.pop()
+# website.push('ibm')
+# print(website.search('amazon'))
+# print(f"length: {website.length}\ntop: {website.top}\nbottom: {website.bottom}")
+
+# instantiated Stack2 class
+books = Stack2()
+# 
+# books.push("Rich Dad Poor Dad")
+# books.push("The Alchemist")
+# books.push("Think And Grow Rich")
+# books.push("A Century Is Not Enough")
+# books.push("Wings Of Fire")
+# books.pop()
+# print(books.peek())
+# print(books.stack)
